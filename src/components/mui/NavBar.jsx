75b6c92197fd35bd12acc60f8273d200384ac536
Simon Dahal <simon.dahal@coderacademy.edu.au>
2022-11-25 13:39:57 +1100

    Week 7 Day 2 - React Forms

--- src/components/mui/NavBar.jsx
+++ src/components/mui/NavBar.jsx
@@ -1,37 +1,44 @@
 import { AppBar, Box, Button, Container, Toolbar } from "@mui/material"
 
 function NavBar() {
+    const navBarItems = [
+        {
+            title: "Products",
+            id: "#products"
+        },
+        {
+            title: "Add Product",
+            id: "#addProduct",
+        },
+        {
+            title: "Cart",
+            id: "#cart"
+        }
+    ]
     return (
         <AppBar position="static">
             <Container maxWidth="xl">
                 <Toolbar disableGutters>
                     <Box sx={{ flexGrow: 1, display: "flex" }}>
-                        <a style={{
-                            textDecoration: 'none'
-                        }} href="#products">
-                            <Button
-                                sx={{
-                                    my: 2,
-                                    color: "white",
-                                    display: "block",
-                                }}
-                            >
-                                Products
-                            </Button>
-                        </a>
-                        <a style={{
-                            textDecoration: 'none'
-                        }} href="#cart">
-                            <Button
-                                sx={{
-                                    my: 2,
-                                    color: "white",
-                                    display: "block",
-                                }}
-                            >
-                                Cart
-                            </Button>
-                        </a>
+                        {
+                            navBarItems.map((item) => {
+                                return (
+                                    <a key={item.title} style={{
+                                        textDecoration: 'none'
+                                    }} href={item.id}>
+                                        <Button
+                                            sx={{
+                                                my: 2,
+                                                color: "white",
+                                                display: "block",
+                                            }}
+                                        >
+                                            {item.title}
+                                        </Button>
+                                    </a>
+                                )
+                            })
+                        }
                     </Box>
                 </Toolbar>
             </Container>
